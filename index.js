4ca91c6b4f2dc99669bfe7568d2f2fff01b2d06a
lutangar <johan.dufour@gmail.com>
2017-12-06 11:39:08 +0100

    cast to number if number else to string

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import isNumber from 'lodash.isnumber';
+
 export default env =>
   Object.keys(env)
     .filter(key => key.indexOf('npm_package_config') !== -1)
-    .map(key => ({ [key]: env[key] }))
+    .map(key => ({ [key]: isNumber(env[key]) ? +env[key] : String(env[key]) }))
     .reduce(
       (result, item) => {
         const key = Object.keys(item)[0];
--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@ import extractPackageConfig from '../index';
 
 const processEnv = {
   npm_package_config_domain: 'localhost',
+  npm_package_config_port: 3000,
   NODE_ENV: 'production',
   another_env_key: 'another_env_value'
 };
@@ -10,5 +11,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(extractPackageConfig(processEnv)).toEqual({
     NODE_ENV: 'production',
     DOMAIN: 'localhost',
+    PORT: 3000,
   });
 });
